49e4388e9352c2cabd478976594a7e8841fb1a6c
Jonathan Grieve <mail@jonniegrieve.co.uk>
2022-05-26 09:25:45 +0100

    setup the basic loop

--- wp-content/themes/jgdm-woocommerce-selectors/index.php
+++ wp-content/themes/jgdm-woocommerce-selectors/index.php
@@ -1,3 +1,51 @@
+
+<?php  
+
+    // page variables
+    $page_title = "JGDM WordPress Selectors"; 
+    $html_title = "JGDM WordPress Selectors";
+    $page_description = "";
+    $page_keywords = "";
+    $page_image = "";
+
+    $page_url = "https://www.jonniegrieve.co.uk";
+
+?>
+
+
+<?php include "template-parts/inc/header.php" ?>
+
 <?php
 
-echo "index.php";
+    // WP Query
+    $banner_args = array(
+
+        'post_type' => '',
+        'post_count' => 6
+
+    );
+
+    $banners = new WP_Query( $banner_args );  
+
+?>   
+
+    <h1>Website Title</h1>
+
+    <?php if ( have_posts() ) : while( have_posts() ) : the_post() ?>
+
+        <h2> <?php the_title();  ?> </h2>
+
+        <p> <?php the_content(); ?> </p>
+
+    <?php endwhile; else: ?>
+
+        <p>No Content Available</p>
+
+    <?php endif; ?>  
+        
+
+<?php include "template-parts/inc/footer.php" ?> 
+
+<?php echo "index.php"; ?>
+
+
