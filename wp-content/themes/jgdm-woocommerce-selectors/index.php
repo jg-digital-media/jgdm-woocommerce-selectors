9a42a96e3d7e32b86b031b132055ee7bd298492c
jg-digital-media <mail@jonniegrieve.co.uk>
2022-05-27 15:55:33 +0100

    style update index, header and footer

--- wp-content/themes/jgdm-woocommerce-selectors/index.php
+++ wp-content/themes/jgdm-woocommerce-selectors/index.php
@@ -30,15 +30,19 @@
 
 ?>   
 
-    <h1>Website Title</h1>
+    <h1>JGDM WooCommerce Selectors</h1>
 
-    <a href="/jgdm-woocommerce-selectors/shop" target="blank">Shop</a>
+    <a href="/jgdm-woocommerce-selectors/shop" class="shop_title" target="blank">Shop</a>
 
     <?php if ( have_posts() ) : while( have_posts() ) : the_post() ?>
+
+    <div class="content_container">
 
         <h2> <?php the_title();  ?> </h2>
 
         <p> <?php the_content(); ?> </p>
+        
+    </div>
 
     <?php endwhile; else: ?>
 
--- wp-content/themes/jgdm-woocommerce-selectors/template-parts/inc/footer.php
+++ wp-content/themes/jgdm-woocommerce-selectors/template-parts/inc/footer.php
@@ -1,7 +1,11 @@
 
 <?php wp_footer();  ?>
 
-<p> footer <?php date("Y"); ?> </p>
+<footer>
+
+    <p>Copyright &copy; Jonnie Grieve Digital Media <a href="https://www.jonniegrieve.co.uk" target="blank">Website</a>  <?php date("Y"); ?> </p>
     
+</footer>
+
 </body>
 </html>
