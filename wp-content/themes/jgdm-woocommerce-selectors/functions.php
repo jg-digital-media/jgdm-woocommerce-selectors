1aa32ebed951bbd61e676a345dba9be641a29571
Jonathan Grieve <mail@jonniegrieve.co.uk>
2022-05-30 14:51:04 +0100

    add the stylesheet path

--- wp-content/themes/jgdm-woocommerce-selectors/functions.php
+++ wp-content/themes/jgdm-woocommerce-selectors/functions.php
@@ -64,7 +64,7 @@
         
         // ENQUEUE MAIN STYLESHEETS
 
-        wp_enqueue_style( 'style', get_template_directory_uri() . "style.css" ); 
+        wp_enqueue_style( 'style', get_template_directory_uri() . "/style.css" ); 
     
         
     }
